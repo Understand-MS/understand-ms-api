a07530ff809ac9a2efa4a17ff6d6b26c2369bebd
Juan Ramos <ptjuanramos@gmail.com>
2026-03-13 11:34:10 +0000

    container id

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,6 +35,7 @@ module "api" {
   source                        = "./modules/container-app"
   app_name                       = var.app_name
   resource_group_name            = "${var.app_name}-rg"
+  container_app_environment_id   = azurerm_container_app_environment.env.id
   container_image                = var.container_image
 
   depends_on = [
