30216adad8fe372ec94d1fe625bfccb7d22b11b8
Juan Ramos <ptjuanramos@gmail.com>
2026-03-13 12:51:06 +0000

    remove boostrap resource creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "main" {
+data "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
 }
@@ -35,28 +35,5 @@ module "api" {
 
   depends_on = [
     azurerm_resource_group.main
-  ]
-}
-
-resource "azurerm_storage_account" "tfstate" {
-  name                     = "umstfstate001"
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  depends_on = [
-    azurerm_resource_group.main
-  ]
-}
-
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.tfstate.name
-  container_access_type = "private"
-
-  depends_on = [
-    azurerm_resource_group.main,
-    azurerm_storage_account.tfstate
   ]
 }
