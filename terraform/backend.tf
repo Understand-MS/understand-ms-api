afdf998dbb5d42ff2e89099f3226eeda6fcf0b98
Juan Ramos <ptjuanramos@gmail.com>
2026-03-13 12:03:54 +0000

    fix container image

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -6,12 +6,12 @@ terraform {
     }
   }
 
-  # backend "azurerm" {
-  #   resource_group_name  = "understand-ms-rg"
-  #   storage_account_name = "understandmstfstateaccount"
-  #   container_name       = "tfstate"
-  #   key                  = "terraform.tfstate"
-  # }
+  backend "azurerm" {
+    resource_group_name  = "understand-ms-rg"
+    storage_account_name = "understandmstfstateaccount"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
