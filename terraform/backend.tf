1b782fd4371e371ea51ac84e5c36b42cd105bd21
ptjuanramos <ptjuanramos@gmail.com>
2026-03-13 03:14:18 +0100

    Fix backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -7,8 +7,8 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = var.resource_group_name
-    storage_account_name = "${var.app_name}tfstateaccount"
+    resource_group_name  = "understand-ms-rg"
+    storage_account_name = "understandmstfstateaccount"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
